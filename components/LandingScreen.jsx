22928fc9f32946fa956c41ad3022ac2118789e9d
Sam <sam.amistoso@gmail.com>
2022-04-12 20:37:16 +1000

    Clean up unnecessary files and landing page message.

--- components/LandingScreen.jsx
+++ components/LandingScreen.jsx
@@ -5,8 +5,8 @@ const LandingScreen = () => {
   return (
     <div className="flex flex-col mt-16">
       <h3 className="text-sm sm:text-lg">
-        Welcome to the wather forecast web application. Please login with your
-        Github user to use the application and view the weather in your city
+        Welcome to the wather forecast web application. Please login to use the
+        application and view the weather in your city
       </h3>
       <a href="/api/auth/login" className="mt-6">
         <Button>Login</Button>
